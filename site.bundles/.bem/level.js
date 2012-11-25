7b6ead69efb1fdb20d244583370e6677fac6c628
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-11-25 05:29:12 +0400

    site.bundles config for plain list of sub pages

--- site.bundles/.bem/level.js
+++ site.bundles/.bem/level.js
@@ -1,1 +1,19 @@
 exports.baseLevelPath = require.resolve('../../.bem/levels/bundles.js');
+
+exports['get-elem'] = function(block, elem) {
+    return [block, elem].join('/');
+};
+
+exports['match-elem'] = function(path) {
+    var m = this.matchRe(),
+        match = (new RegExp([ '^(' + m + ')',
+            '(' + m + ')(.*?)$'].join('/'))).exec(path);
+
+    if (!match) return false;
+
+    return {
+        block: match[1],
+        elem: match[2],
+        suffix: match[3]
+    };
+};
