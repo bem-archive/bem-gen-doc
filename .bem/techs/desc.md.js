6fa49e4431232192b942208c2e2c94ff72ce922c
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-11-25 05:23:25 +0400

    i18n techs refactoring

--- .bem/techs/desc.md.js
+++ .bem/techs/desc.md.js
@@ -1,38 +1,15 @@
 var BEM = require('bem'),
-    Q = BEM.require('qq'),
-    
-    DEFAULT_LANGS = ['ru', 'en'];
+    Q = BEM.require('qq');
     
 
-exports.techMixin = {
+exports.techMixin = BEM.util.extend(require('./i18n.js').LangMixin, {
         
-    getLangs: function() {
-        var env = process.env.BEM_I18N_LANGS;
-        return env? env.split(' ') : [].concat(DEFAULT_LANGS);
-    },
-
-    getDefaultLang: function() {
-        return process.env.BEM_I18N_DEFAULT_LANG || this.getLangs().shift();
-    },
-
-    getSuffixForLang: function(lang) {
+    getSuffixForLang : function(lang) {
         return [lang, 'md'].join('.');
     },
     
     getSuffixes : function() {
         return this.getLangs().map(this.getSuffixForLang, this);
-    },
-
-    // XXX: Move to IntrospectNode
-    readAllContent : function(prefix) {
-
-        var res = {},
-            suffix = this.getSuffixForLang(this.getDefaultLang());
-
-        res[this.getTechName()] = this.readContent(this.getPath(prefix, suffix), suffix);
-
-        return Q.shallow(res);
-
     }
-
-};
+    
+});
