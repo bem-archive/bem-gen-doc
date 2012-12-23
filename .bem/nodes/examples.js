1264613f41948b6190c459887318cc77f3c9f43b
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-12-24 03:59:03 +0400

    Examples node added (hardcode)

---
+++ .bem/nodes/examples.js
@@ -0,0 +1,275 @@
+/**
+ * @module nodes/examples
+ */
+
+var PATH = require('path'),
+    BEM = require('bem'),
+    LOGGER = require('bem/lib/logger'),
+
+    registry = require('bem/lib/nodesregistry'),
+    BundlesLevelNodeName = require('bem/lib/nodes/level').BundlesLevelNodeName,
+    BundleNodeName = require('bem/lib/nodes/bundle').BundleNodeName,
+    Q = BEM.require('q'),
+
+    /** @type Function */
+    createLevel = BEM.createLevel,
+    /** @type Function */
+    U = BEM.util,
+
+    ExamplesNodeName = exports.ExamplesNodeName = 'MachineExamplesNode',
+
+    // FIXME: hardcode
+    NODE_ID = 'examples*',
+
+    /**
+     * @type Function
+     * @params {Any}
+     */
+    log = function() {
+        /** @type Function */
+        var inspect = require('util').inspect;
+        [].slice.call(arguments).forEach(function(o) {
+            console.log(inspect(o, false, null));
+        });
+    };
+
+
+Object.defineProperty(exports, ExamplesNodeName, {
+    get : function() {
+        return registry.getNodeClass(ExamplesNodeName);
+    }
+});
+
+
+registry.decl(ExamplesNodeName, 'Node', {
+
+    __constructor : function(o) {
+
+        this.__base.apply(this, arguments);
+
+        this.root = o.root;
+
+        /** где искать примеры */
+        this.levels = o.levels;
+        /** куда складывать примеры */
+        //this.output = o.output;
+        // FIXME: hardcode
+        this.output = PATH.join('release', 'examples');
+
+    },
+
+    filterExamplesPaths : function() {
+
+        var examples = [];
+
+        this.levels.forEach(function(/** String */ levelPath) {
+            var level = createLevel(levelPath);
+
+            [].push.apply(examples, level.getItemsByIntrospection()
+                    .filter(function(item) {
+                        return item.tech === 'examples';
+                    })
+                    .map(function(item) {
+                        item.level = level.dir;
+                        return item;
+                    }));
+        });
+
+        return Q.all(examples);
+
+    },
+
+    getOutputPath : function() {
+
+        return PATH.resolve(this.root, this.output);
+
+    },
+
+    getBaseOutputLevel : function() {
+        return PATH.resolve(__dirname, '../levels/sets');
+    },
+
+    getOutputLevel : function() {
+
+        if(!this._output)
+            this._output = createLevel(this.getOutputPath());
+
+        return this._output;
+
+    },
+
+    /**
+     * @param root
+     * @param [base]
+     * @param [force]
+     * @returns {Q.promise}
+     */
+    makeLevel : function(root, base, force) {
+
+        if(force == null && typeof base === 'boolean') {
+            force = base;
+            base = null;
+        }
+
+        var opts = {
+                outputDir : PATH.dirname(root),
+                force : force
+            },
+            name = PATH.basename(root);
+
+        base && (opts.level = base);
+
+        return BEM.api.create.level(opts, { names : [name] });
+
+    },
+
+    copyExampleToOutput : function(bemitem) {
+
+        var _this = this,
+            level = createLevel(bemitem.level),
+            outLevel = this.getOutputLevel(),
+            outPath = outLevel.getPathByObj(bemitem, bemitem.tech);
+
+        level = createLevel(level.getPathByObj(bemitem, bemitem.tech));
+
+        return this.makeLevel(outPath, level.dir, true)
+            .then(function() {
+
+                outLevel = createLevel(outPath);
+
+                return Q.all(level.getItemsByIntrospection()
+                    .filter(function(item) {
+                        // FIXME: hardcode
+                        return ~['bemjson.js'].indexOf(item.tech);
+                    })
+                    .map(function(item) {
+
+                        var src = level.getPathByObj(item, item.tech),
+                            desc = outLevel.getPathByObj(item, item.tech);
+
+                        return _this._storeExampleToDesc(src, desc);
+
+                    }));
+
+            });
+
+    },
+
+    _storeExampleToDesc : function(src, desc) {
+
+        U.mkdirp(PATH.dirname(desc));
+
+        return U.readFile(src)
+            .then(function(data) {
+
+                return U.writeFileIfDiffers(desc, data)
+                    .then(function() {
+                        return desc;
+                    });
+
+            });
+
+    },
+
+    make : function() {
+
+        var _this = this,
+            ctx = this.ctx;
+
+        return this.filterExamplesPaths()
+            .then(function(items) {
+
+                // FIXME: hardcode
+                return _this.makeLevel(_this.getOutputPath(),
+                        require.resolve(_this.getBaseOutputLevel()))
+                    .then(function() {
+
+                        return Q.all(items.map(function(item) {
+                            return _this.copyExampleToOutput(item);
+                        }));
+
+                    });
+
+            })
+            .then(function() {
+
+                return Q.all(_this.getOutputLevel().getItemsByIntrospection()
+                    .filter(function(item) {
+                        return item.tech === 'examples';
+                    })
+                    .map(function(item) {
+
+                        var path = _this.getOutputLevel().getPathByObj(item, item.tech),
+                            arch = ctx.arch,
+                            node = new (registry.getNodeClass('MachineExamplesLevel'))({
+                                root    : _this.root,
+                                level   : createLevel(path)
+                            });
+
+                        arch.setNode(node, arch.getParents(_this));
+
+                        return node.getId();
+
+                    }));
+
+            })
+            .done();
+
+    }
+
+}, {
+
+    createId : function(o) {
+        return NODE_ID;
+    }
+
+});
+
+
+registry.decl('MachineExamplesLevel', BundlesLevelNodeName, {
+
+    itemNodeClassName : 'MachineExampleNode'
+
+});
+
+
+registry.decl('MachineExampleNode', BundleNodeName, {
+
+    /**
+     * FIXME: hardcode
+     * @returns {Array}
+     */
+    getTechs : function() {
+
+        return [
+            'bemjson.js',
+            'bemdecl.js',
+            'deps.js',
+            'bemhtml.js',
+            'css',
+            'ie.css',
+            'js',
+            'html'
+        ];
+
+    },
+
+    /**
+     * FIXME: hardcode
+     * @returns
+     */
+    getLevels : function() {
+
+        return ([
+                'lib/bem-bl/blocks-common',
+                'lib/bem-bl/blocks-desktop',
+                'desktop.blocks'
+            ])
+            .map(function(level) {
+                return PATH.resolve(this.root, level);
+            }, this)
+            .concat([PATH.resolve(this.root, PATH.dirname(this.getNodePrefix()), 'blocks')]);
+
+    }
+
+});
