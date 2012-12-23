9b5e54303d2938707d55c3c5b7f47ce36da6eaae
Vladimir Varankin <nek.narqo+git@gmail.com>
2012-12-24 03:58:34 +0400

    Examples levels proto

--- .bem/levels/examples.js
+++ .bem/levels/examples.js
@@ -1,11 +1,26 @@
+var PATH = require('path'),
+
+    pjoin = PATH.join,
+    presolve = PATH.resolve.bind(null, __dirname),
+
+    LIB_ROOT = presolve('../../lib'),
+
+    PRJ_TECHS = presolve('../techs'),
+    BEMBL_TECHS = pjoin(LIB_ROOT, 'bem-bl/blocks-common/i-bem/bem/techs');
+
+
 exports.baseLevelPath = require.resolve('./bundles.js');
+
 
 exports.getTechs = function() {
 
     return require('BEM').util.extend(this.__base() || {}, {
         'blocks'        : '',
         'bemjson.js'    : '',
-        'title.txt'     : ''
+        'title.txt'     : '',
+
+        'bemhtml.js'    : pjoin(BEMBL_TECHS, 'bemhtml'),
+        'html'          : pjoin(BEMBL_TECHS, 'html')
     });
 
 };
---
+++ .bem/levels/sets.js
@@ -0,0 +1,9 @@
+exports.baseLevelPath = require.resolve('bem/lib/levels/simple');
+
+exports.getTechs = function() {
+
+    return {
+        'examples' : ''
+    };
+
+};
