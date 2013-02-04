7e3539712e5ecabd982d018c08622727834e8b77
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-02-04 16:23:27 +0400

    Use ENV_ROOT as lib root storage

--- .bem/environ.js
+++ .bem/environ.js
@@ -35,7 +35,7 @@ var PATH = require('path'),
      * @type String
      * @exports LIB_DIR
      */
-    LIB_DIR = exports.LIB_DIR = 'vendor',
+    LIB_DIR = exports.LIB_DIR = '.',
 
     /**
      * Путь до корня хранилища библиотек
