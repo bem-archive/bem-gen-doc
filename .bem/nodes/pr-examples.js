bbe40d165517ffb877d805214da988429086e21f
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-12 21:49:42 +0400

    examples modules is deprecated, should use pr-examples

---
+++ .bem/nodes/pr-examples.js
@@ -0,0 +1,46 @@
+var PATH = require('path'),
+    BEM = require('bem'),
+
+    registry = require('bem/lib/nodesregistry'),
+    environ = require('../environ'),
+
+    setsNodes = require(environ.getLibPath('bem-pr', 'bem/nodes/sets.js'));
+
+
+registry.decl(setsNodes.ExampleNodeName, {
+
+    getMachineExamplesTechs : function() {
+        return [
+            'bemjson.js'
+        ];
+    },
+
+    getMachineExamplesLevels : function() {
+
+        return [];
+
+    },
+
+    getTechs : function() {
+
+        var bundle = PATH.relative(this.root, this.getPath());
+        if(bundle.indexOf('release/examples') === 0) {
+            return this.getMachineExamplesTechs();
+        }
+
+        return this.__base.apply(this, arguments);
+
+    },
+
+    getLevels : function() {
+
+        var bundle = PATH.relative(this.root, this.getPath());
+        if(bundle.indexOf('release/examples') === 0) {
+            return this.getMachineExamplesLevels();
+        }
+
+        return this.__base.apply(this, arguments);
+
+    }
+
+});
