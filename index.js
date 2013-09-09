96f15479cd58654954e1e67d06a6956193655f8b
Sergey Belov <peimei@ya.ru>
2013-09-09 18:58:11 +0300

    Add resolveTech() and resolveLevel() function to module export

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ exports.extendMake = function(registry) {
     require('./.bem/nodes')(registry);
 };
 
+/**
+ * Resolve path to the `techName` tech module
+ * @param techName
+ * @returns {String}
+ */
+exports.resolveTech = function(techName) {
+    return require.resolve('./.bem/techs/' + techName);
+};
+
+/**
+ * Resolve path to the `levelName` level config module
+ * @param levelName
+ * @returns {String}
+ */
+exports.resolveLevel = function(levelName) {
+    return require.resolve('./.bem/levels/' + levelName);
+};
+
+// not recommended to use, package structure could change at any time
 exports.require = require;
 exports.resolve = PATH.resolve.bind(PATH, __dirname);
